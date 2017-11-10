7e2f2964823db2b1f3054612e0f86450f55940ee
Sirazutdinov Airat <siraz@yandex.ru>
2017-11-10 12:50:24 +0300

    Add columns to users list

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   namespace :admin do
-    resources :users, only: :index
+    resources :users, only: [ :index, :update ]
     mount ActionCable.server => '/cable'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
